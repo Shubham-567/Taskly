d08680028c9777569098eb02f738725a2bc46dc2
Shubham <shubhampatil1356@gmail.com>
2025-09-01 20:42:48 +0530

    Backend: add auth routes, controller and jwt middleware

---
+++ backend/controllers/authController.js
@@ -0,0 +1,105 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+// generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // check if all fields exists
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    if (user) {
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.error("Register user error: ", error.message);
+    res.status(500).json({ message: "server error" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if email and pass exists
+    if (!email || !password) {
+      res.status(400).json({ message: "Email and Password are required" });
+    }
+
+    // find user
+    const user = await User.findOne({ email }).select("+password");
+
+    let isPassMatch = false;
+
+    if (user) {
+      isPassMatch = await bcrypt.compare(password, user.password);
+    }
+
+    if (user && isPassMatch) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(400).json({
+        message: "Invalid email or password",
+      });
+    }
+  } catch (error) {
+    console.error("Login error: ", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    }
+
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } catch (error) {
+    console.error("Get user profile error: ", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
