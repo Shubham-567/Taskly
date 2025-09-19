4e8baf8da03b67a72202fed92fde22a9183f8d80
Shubham <shubhampatil1356@gmail.com>
2025-09-19 20:22:41 +0530

    Frontend: add confirmation moadal for deletion in TaskCard

---
+++ frontend/src/components/ConfirmationModal.jsx
@@ -0,0 +1,33 @@
+import { AlertTriangle } from "lucide-react";
+
+const ConfirmationModal = ({ onClose, onConfirm }) => {
+  return (
+    <div className='bg-bg/60 backdrop-blur-[1px] fixed top-0 left-0 w-full h-full flex justify-center items-center'>
+      <div className='bg-surface rounded-lg shadow-lg p-6 flex flex-col items-center justify-center gap-2 w-full max-w-sm'>
+        <AlertTriangle className='size-8 text-danger-500' />
+        <h3 className='text-2xl font-semibold text-txt-primary'>
+          Confirm Deletion
+        </h3>
+        <p className='text-txt-secondary text-center'>
+          Are you sure you want to delete the task? This action cannot be
+          undone.
+        </p>
+
+        <div className='flex justify-center gap-2 mt-2'>
+          <button
+            className='py-2 px-4 rounded-md border border-border bg-surface-secondary hover:opacity-80 cursor-pointer font-medium text-base'
+            onClick={onClose}>
+            Cancel
+          </button>
+          <button
+            className='py-2 px-4 rounded-md border border-border bg-danger-500 text-danger-foreground hover:opacity-80 cursor-pointer font-medium text-base'
+            onClick={onConfirm}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConfirmationModal;
--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,57 +1,66 @@
 import { Pen, Trash2 } from "lucide-react";
 import useTaskStore from "../store/taskStore";
+import ConfirmationModal from "./ConfirmationModal";
+import { useState } from "react";
 
 const TaskCard = ({ task, openModal }) => {
   const { removeTask, toggleTaskStatus } = useTaskStore();
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
 
   if (!task) return null;
 
   return (
-    <div className='bg-surface border border-border rounded-md flex flex-col justify-between gap-2 p-4'>
-      <div className=''>
-        <label
-          htmlFor={task._id + "-status"}
-          className='flex items-start justify-between gap-2'>
-          <h3
-            className={`text-lg font-semibold text-txt-primary cursor-pointer ${
+    <>
+      <div className='bg-surface border border-border rounded-md flex flex-col justify-between gap-2 p-4'>
+        <div className=''>
+          <label
+            htmlFor={task._id + "-status"}
+            className='flex items-start justify-between gap-2'>
+            <h3
+              className={`text-lg font-semibold text-txt-primary cursor-pointer ${
+                task.status === "completed"
+                  ? " line-through text-txt-secondary/70"
+                  : ""
+              }`}>
+              {task.title}
+            </h3>
+
+            <input
+              type='checkbox'
+              id={task._id + "-status"}
+              checked={task.status === "completed"}
+              onChange={() => toggleTaskStatus(task._id)}
+              className='mt-2 accent-primary-500 '
+            />
+          </label>
+
+          <p
+            className={`text-base text-txt-secondary mt-1 min-h-[6rem] ${
               task.status === "completed"
-                ? " line-through text-txt-secondary/70"
+                ? " line-through text-txt-secondary/90"
                 : ""
             }`}>
-            {task.title}
-          </h3>
+            {task.description}
+          </p>
+        </div>
 
-          <input
-            type='checkbox'
-            id={task._id + "-status"}
-            checked={task.status === "completed"}
-            onChange={() => toggleTaskStatus(task._id)}
-            className='mt-2 accent-primary-500 '
-          />
-        </label>
-
-        <p
-          className={`text-base text-txt-secondary mt-1 min-h-[6rem] ${
-            task.status === "completed"
-              ? " line-through text-txt-secondary/90"
-              : ""
-          }`}>
-          {task.description}
-        </p>
+        <div className='border-t border-border p-4 pb-0 flex items-center justify-end gap-4'>
+          <button onClick={openModal}>
+            <Pen className='size-4 text-txt-secondary hover:text-primary-500 cursor-pointer' />
+          </button>
+          <button onClick={() => setShowConfirmationModal(true)}>
+            <Trash2 className='size-4 text-txt-secondary hover:text-primary-500 cursor-pointer' />
+          </button>
+        </div>
       </div>
 
-      <div className='border-t border-border p-4 pb-0 flex items-center justify-end gap-4'>
-        <button onClick={openModal}>
-          <Pen className='size-4 text-txt-secondary hover:text-primary-500 cursor-pointer' />
-        </button>
-        <button
-          onClick={async () => {
-            await removeTask(task._id);
-          }}>
-          <Trash2 className='size-4 text-txt-secondary hover:text-primary-500 cursor-pointer' />
-        </button>
-      </div>
-    </div>
+      {showConfirmationModal ? (
+        <ConfirmationModal
+          onClose={() => setShowConfirmationModal(false)}
+          onConfirm={() => removeTask(task._id)}
+        />
+      ) : null}
+    </>
   );
 };
 
